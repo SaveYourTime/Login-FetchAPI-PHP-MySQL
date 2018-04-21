ffa99733441c2159fffc3b6cea00cb229a4ba14a
Aaron Lu <aaron_lu1@yahoo.com>
2018-04-21 14:32:28 +0800

    bug fix

--- cmd.php
+++ cmd.php
@@ -1,5 +1,7 @@
 <?php
 header("Content-Type: text/html, application/json; charset=utf-8;");
+mb_internal_encoding('UTF-8');
+mb_http_output('UTF-8');
 require "cmd_lib.php";
 
 $input = json_decode(file_get_contents("php://input"));
--- js/web_lib.js
+++ js/web_lib.js
@@ -1,42 +1,40 @@
-/*
-	Copyright Aaron Lu
-	Created by Aaron Lu
-*/
-
-function easyPost(url, objData, callback, errorCallback, timeout) {
-	timeout = timeout > 0 && timeout != undefined ? timeout : 5000;
+function easyPost(url, objData, successCallback, errorCallback) {
 	$.ajax({
 		type: 'POST',
 		url: url,
-		data: {
-			json: JSON.stringify(objData)
-		},
+		data: JSON.stringify(objData),
 		dataType: 'json',
-		success: function (xhr) {
-			if (callback!==undefined) callback(xhr);
+		success: function (data) {
+			if (successCallback !== undefined) successCallback(data);
 		},
 		error: function (xhr, status, error) {
-			console.log({'errorMsg': status, 'readyStatus': xhr['readyState'], 'status': xhr['status'], 'statusText': xhr['statusText']});
-			if (errorCallback!==undefined) errorCallback(error);
-		},
-		timeout: timeout
+			console.log({
+				'errorMsg': status,
+				'readyStatus': xhr['readyState'],
+				'status': xhr['status'],
+				'statusText': xhr['statusText']
+			});
+			if (errorCallback !== undefined) errorCallback(error);
+		}
 	});
 }
+
 function easyFetch(url, objData, successCallback, errorCallback) {
 	fetch(url, {
 		method: 'POST',
-		headers: new Headers({
+		headers: {
 			'Accept': 'application/json',
 			'Content-Type': 'application/json',
-		}),
+		},
 		body: JSON.stringify(objData),
+		credentials: 'same-origin'
 	})
 		.then(response => response.json())
 		.then(data => {
-			successCallback(data);
+			if (successCallback !== undefined) successCallback(data);
 		})
 		.catch(error => {
-			console.log('catched error!');
-			if (errorCallback!==undefined) errorCallback(error);
+			console.log(error);
+			if (errorCallback !== undefined) errorCallback(error);
 		});
 }
